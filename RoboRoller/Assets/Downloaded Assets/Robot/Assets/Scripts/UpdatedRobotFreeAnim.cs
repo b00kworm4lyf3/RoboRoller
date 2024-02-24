342d95977fdd9426865b2497b55a284e66de142b
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2024-02-23 17:12:41 -0800

    Player movement tests

---
+++ RoboRoller/Assets/Downloaded Assets/Robot/Assets/Scripts/UpdatedRobotFreeAnim.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UpdatedRobotFreeAnim : MonoBehaviour {
+
+	Vector3 rot = Vector3.zero;
+	float rotSpeed = 40f;
+	float speed = 3.5f;
+	Animator anim;
+	float movementX;
+	float movementY;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		anim = gameObject.GetComponent<Animator>();
+		gameObject.transform.eulerAngles = rot;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		Vector3 movement = new Vector3(movementX, 0, movementY);
+        rb.AddForce(movement * speed);
+
+		CheckKey();
+		gameObject.transform.eulerAngles = rot;
+	}
+
+	private void OnMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+	void CheckKey()
+	{
+		// Walk
+		if (Input.GetKey(KeyCode.W))
+		{
+			anim.SetBool("Walk_Anim", true);
+		}
+		else if (Input.GetKeyUp(KeyCode.W))
+		{
+			anim.SetBool("Walk_Anim", false);
+		}
+
+		// Rotate Left
+		if (Input.GetKey(KeyCode.A))
+		{
+			rot[1] -= rotSpeed * Time.fixedDeltaTime;
+		}
+
+		// Rotate Right
+		if (Input.GetKey(KeyCode.D))
+		{
+			rot[1] += rotSpeed * Time.fixedDeltaTime;
+		}
+
+		// Roll
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (anim.GetBool("Roll_Anim"))
+			{
+				anim.SetBool("Roll_Anim", false);
+			}
+			else
+			{
+				anim.SetBool("Roll_Anim", true);
+			}
+		}
+
+		// Close
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			if (!anim.GetBool("Open_Anim"))
+			{
+				anim.SetBool("Open_Anim", true);
+			}
+			else
+			{
+				anim.SetBool("Open_Anim", false);
+			}
+		}
+	}
+
+}
